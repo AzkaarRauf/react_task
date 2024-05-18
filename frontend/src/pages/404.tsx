da62403e7905c1b644d0b969fc7eaccf22c1da84
Azkaar Khatib <azkaar.khatib@nntsoftware.in>
2024-05-18 15:26:39 +0530

    chore: Update 404 page layout and styles

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -2,15 +2,23 @@ import { Link } from "react-router-dom"
 
 export default function NotFound() {
   return (
-    <div className="bg-base-100 flex content-center">
-      <div className="card">
-        <div className="card-body">
-          404 Not Found
-          <Link to="/" className="btn btn-primary">
-            Go Home
+    <section className="flex items-center h-screen p-16 bg-gray-50 dark:bg-gray-700">
+      <div className="container flex flex-col items-center ">
+        <div className="flex flex-col gap-6 max-w-md text-center">
+          <h2 className="font-extrabold text-9xl text-gray-600 dark:text-gray-100">
+            <span className="sr-only">Error</span>404
+          </h2>
+          <p className="text-2xl md:text-3xl dark:text-gray-300">
+            Sorry, we couldn't find this page.
+          </p>
+          <Link
+            to="/"
+            className="px-8 py-4 text-xl font-semibold rounded bg-purple-600 text-gray-50 hover:text-gray-200"
+          >
+            Back to home
           </Link>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
